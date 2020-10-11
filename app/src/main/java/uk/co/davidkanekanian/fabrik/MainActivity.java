aef848da812e2c7700612bf4823d75faa4730735
David Kanekanian <dkanekanian1@gmail.com>
2020-10-11 14:25:12 +0100

    Initial commit 🏀

---
+++ app/src/main/java/uk/co/davidkanekanian/fabrik/MainActivity.java
@@ -0,0 +1,14 @@
+package uk.co.davidkanekanian.fabrik;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
