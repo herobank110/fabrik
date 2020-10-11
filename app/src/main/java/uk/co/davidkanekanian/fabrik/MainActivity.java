d3a7de7afda587a19b93e61121598c5a851e8aef
David Kanekanian <dkanekanian1@gmail.com>
2020-10-11 15:33:12 +0100

    Add JOML for basic vector math dependency

--- app/src/main/java/uk/co/davidkanekanian/fabrik/MainActivity.java
+++ app/src/main/java/uk/co/davidkanekanian/fabrik/MainActivity.java
@@ -3,6 +3,11 @@ package uk.co.davidkanekanian.fabrik;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.joml.Vector2f;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Vector2f a = new Vector2f(2.25f, 0.f);
+        Vector2f b = new Vector2f(5.25f, 10.f);
+        a.add(b);
+
+        TextView textView = findViewById(R.id.test_text);
+        textView.setText(a.toString(NumberFormat.getNumberInstance()));
     }
 }
